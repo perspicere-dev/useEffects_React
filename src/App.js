cc32ca780a8c8ddc17a08176a147cec6f34f2392
Przemysław Wnęk <wnek.przemek@gmail.com>
2022-02-12 13:10:47 +0100

    add useEffect hook for store logIn info and logout properly

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+  useEffect(() => { //it would run ONLY if the dependencies would chacnge (second argument [] in useEffect hook)
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []); //We want it to check if user is logged in once, when APP starts, so no dependencies needed, they wont change so it wont trigger again the function - that is what we want
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1"); //first arg is string identifier, second string which we store
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
